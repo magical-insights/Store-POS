c111c1c5618bd45267bbf3a5c9764d51fba1ca12
Enyosolutions <fb@enyosolutions.com>
2025-05-15 06:26:36 +0100

    feat: update dependencies
    fix: bug on admin selector

--- start.js
+++ start.js
@@ -1,45 +1,47 @@
 const setupEvents = require('./installers/setupEvents')
- if (setupEvents.handleSquirrelEvent()) {
-    return;
- }
+if (setupEvents.handleSquirrelEvent()) {
+  return
+}
 
-const server = require('./server');
-const {app, BrowserWindow, ipcMain, screen} = require('electron');
+const server = require('./server')
+const { app, BrowserWindow, ipcMain, screen } = require('electron')
 const path = require('path')
+const ElectronStore = require('electron-store')
 
-const contextMenu = require('electron-context-menu');
+require('@electron/remote/main').initialize()
 
+const contextMenu = require('electron-context-menu')
+
+ElectronStore.initRenderer()
 let mainWindow
 
-function createWindow() {
-  var primaryDisplay = screen.getPrimaryDisplay();
-  var screenDimensions = primaryDisplay.workAreaSize;
+function createWindow () {
+  var primaryDisplay = screen.getPrimaryDisplay()
+  var screenDimensions = primaryDisplay.workAreaSize
   mainWindow = new BrowserWindow({
     width: screenDimensions.width,
     height: screenDimensions.height,
     frame: false,
-    minWidth: 1200, 
+    minWidth: 1200,
     minHeight: 750,
-    
+
     webPreferences: {
       nodeIntegration: true,
-      enableRemoteModule: true,
       contextIsolation: false
-    },
-  });
+    }
+  })
 
-  mainWindow.maximize();
-  mainWindow.show();
+  require('@electron/remote/main').enable(mainWindow.webContents)
 
-  mainWindow.loadURL(
-    `file://${path.join(__dirname, 'index.html')}`
-  )
+  mainWindow.maximize()
+  mainWindow.show()
+
+  mainWindow.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
   mainWindow.on('closed', () => {
     mainWindow = null
   })
 }
-
 
 app.on('ready', createWindow)
 
@@ -55,38 +57,32 @@ app.on('activate', () => {
   }
 })
 
-
-
 ipcMain.on('app-quit', (evt, arg) => {
   app.quit()
 })
 
-
 ipcMain.on('app-reload', (event, arg) => {
-  mainWindow.reload();
-});
-
-
+  mainWindow.reload()
+})
 
 contextMenu({
   prepend: (params, browserWindow) => [
-     
-      {label: 'DevTools',
-       click(item, focusedWindow){
-        focusedWindow.toggleDevTools();
+    {
+      label: 'DevTools',
+      click (item, focusedWindow) {
+        focusedWindow.toggleDevTools()
       }
     },
-     { 
-      label: "Reload", 
-        click() {
-          mainWindow.reload();
-      } 
-    // },
-    // {  label: 'Quit',  click:  function(){
-    //    mainWindow.destroy();
-    //     mainWindow.quit();
-    // } 
-  }  
-  ],
-
-});
+    {
+      label: 'Reload',
+      click () {
+        mainWindow.reload()
+      }
+      // },
+      // {  label: 'Quit',  click:  function(){
+      //    mainWindow.destroy();
+      //     mainWindow.quit();
+      // }
+    }
+  ]
+})
